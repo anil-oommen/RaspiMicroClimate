8f7eb5a24e9c7bb6eae6f85993cc600e1720738c
anil-oommen <aniloommen13@yahoo.com>
2016-10-01 10:08:45 +0800

    Cleanup

---
+++ app/README.md
@@ -0,0 +1,12 @@
+
+
+https://github.com/angular/bower-material/#installing-angular-material
+Used NPM to install bower is deprecated.
+
+installed in app/
+npm list
+├── angular@1.5.8
+├── angular-animate@1.5.8
+├── angular-aria@1.5.8
+├── angular-material@1.1.0-rc2
+└── angular-messages@1.5.8
--- app/views.py
+++ app/views.py
@@ -1,31 +1,31 @@
-from flask import render_template, flash, redirect, request, Response
+from flask import render_template, flash, redirect, request, Response ,send_from_directory, send_file
 import json
 from app import app
-from .forms import LoginForm
 from modAppConfig import initConfig
+from time import gmtime, strftime
 
 import sqlite3
 config = initConfig()
 
+curr_nea_weather_code ='NA'
+curr_nea_weather_desc ='NA'
+curr_nea_weather_location ='NA'
+curr_nea_weather_lastfeed = 'NA'
 
+
+curr_dht22_temperature_dc='NA'
+curr_dht22_humidity_pc='NA'
+curr_dht22_lastfeed= 'NA'
+
+curr_acir_power='NA'
+curr_acir_temp='NA'
+curr_acir_mode='NA'
+curr_acir_lastfeed='NA'
 
 
 
 # index view function suppressed for brevity
 
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    print(form.openid.data)
-    if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/login')
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form,
-                           providers=app.config['OPENID_PROVIDERS'])
 
 
 @app.route('/rmc_ws/neaweather', methods=['GET'])
@@ -41,8 +41,20 @@ def rmcWebServiceNEA():
                     ('NEA_CLIMATE', 'weather_desc', weather_desc),
                     ('NEA_CLIMATE', 'weather_location', weather_location)]
         cursor.executemany(
-            ''' INSERT INTO rmc_feed_events(source, param_name, param_value,create_dt,create_time ) VALUES(?,?,?,date('now'),datetime('now'))''',
+            ''' INSERT INTO rmc_feed_events(source, param_name, param_value,create_dt,create_time ) VALUES(?,?,?,date('now', 'localtime'),datetime('now', 'localtime'))''',
         records)
+
+        global curr_nea_weather_code
+        global curr_nea_weather_desc
+        global curr_nea_weather_location
+        global curr_nea_weather_lastfeed
+
+        curr_nea_weather_code=weather_code
+        curr_nea_weather_desc = weather_desc
+        curr_nea_weather_location = weather_location
+        curr_nea_weather_lastfeed = strftime("%b %d %H:%M", gmtime())
+
+
         cursor.close()
         db.commit()
         return "SUCCESS"
@@ -62,8 +74,20 @@ def rmcWebServiceDHT22():
         records = [ ('DHT22_SENSOR', 'temperature_dc', temperature_dc),
                     ('DHT22_SENSOR', 'humidity_pc', humidity_pc)]
         cursor.executemany(
-            ''' INSERT INTO rmc_feed_events(source, param_name, param_value,create_dt,create_time ) VALUES(?,?,?,date('now'),datetime('now'))''',
+            ''' INSERT INTO rmc_feed_events(source, param_name, param_value,create_dt,create_time ) VALUES(?,?,?,date('now', 'localtime'),datetime('now', 'localtime'))''',
         records)
+        global curr_dht22_temperature_dc
+        global curr_dht22_humidity_pc
+        global curr_dht22_lastfeed
+
+        #curr_dht22_temperature_dc = print("{0:.1f}".format(float(temperature_dc)))
+        #curr_dht22_humidity_pc = print("{0:.1f}".format(float(humidity_pc)))
+
+        curr_dht22_temperature_dc = temperature_dc[:4]
+        curr_dht22_humidity_pc = humidity_pc[:4]
+
+        curr_dht22_lastfeed = strftime("%b %d %H:%M", gmtime())
+
         cursor.close()
         db.commit()
         return "SUCCESS"
@@ -84,8 +108,18 @@ def rmcWebServiceAirconIR():
                     ('AIRCON_IR_INPUT', 'temperature_dc', temp),
                     ('AIRCON_IR_INPUT', 'mode', mode)]
         cursor.executemany(
-            ''' INSERT INTO rmc_feed_events(source, param_name, param_value,create_dt,create_time ) VALUES(?,?,?,date('now'),datetime('now'))''',
+            ''' INSERT INTO rmc_feed_events(source, param_name, param_value,create_dt,create_time ) VALUES(?,?,?,date('now', 'localtime'),datetime('now', 'localtime'))''',
         records)
+        global curr_acir_power
+        global curr_acir_temp
+        global curr_acir_mode
+        global curr_acir_lastfeed
+
+        curr_acir_power = power
+        curr_acir_temp = temp
+        curr_acir_mode = mode
+        curr_acir_lastfeed = strftime("%b %d %H:%M", gmtime())
+
         cursor.close()
         db.commit()
         return "SUCCESS"
@@ -132,3 +166,70 @@ def rmcWebServiceQueryFeed():
 
     resp = Response(js, status=200, mimetype='application/json')
     return resp
+
+
+
+@app.route('/rmc_ws/now', methods=['GET'])
+def rmcWebServiceNow():
+    global curr_nea_weather_code
+    global curr_nea_weather_lastfeed
+    global curr_nea_weather_desc
+    global curr_nea_weather_location
+
+    global curr_dht22_temperature_dc
+    global curr_dht22_humidity_pc
+    global curr_dht22_lastfeed
+
+    global curr_acir_power
+    global curr_acir_temp
+    global curr_acir_mode
+    global curr_acir_lastfeed
+    now_data = {
+        "now": []
+    }
+    now_data['now'].append({
+        'nea_weather_code': curr_nea_weather_code,
+        'nea_weather_desc' : curr_nea_weather_desc,
+        'nea_weather_location' : curr_nea_weather_location,
+        'nea_weather_lastfeed': curr_nea_weather_lastfeed,
+
+        'dht22_temperature_dc' :curr_dht22_temperature_dc,
+        'dht22_humidity_pc' : curr_dht22_humidity_pc,
+        'dht22_lastfeed' : curr_dht22_lastfeed,
+
+        'acir_power': curr_acir_power,
+        'acir_temp' : curr_acir_temp,
+        'acir_mode' : curr_acir_mode,
+        'acir_lastfeed' : curr_acir_lastfeed
+
+    })
+    js = json.dumps(now_data)
+
+    resp = Response(js, status=200, mimetype='application/json')
+    return resp
+
+
+
+
+
+# Static Section
+
+@app.route('/static/<path:path>')
+def rmc_static(path):
+    return send_from_directory('static', path)
+
+@app.route('/node_modules/<path:path>')
+def rmc_node_modules(path):
+    return send_from_directory('node_modules', path)
+
+@app.route('/rmc_ws/debug_ir_gpio')
+def rmc_default():
+    print("Delivering default File:gpio_ir_reciever.sysout")
+    return send_file('gpio_ir_reciever.sysout')
+
+
+@app.route('/')
+def rmc_default():
+    print("Delivering default File:index.html")
+    return send_file('index.html')
+
